367cad42cc6e898b7607e37cd67aaf173886a15a
IsraelW <>
2023-01-02 19:03:01 +0000

    Barebone app framework

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from deta import Deta
+import os
+
+app = Flask(__name__)
+deta = Deta(os.environ.get("DETA_PROJECT_KEY"))
+
+words = deta.Base("words")
+
+
+@app.route("/")
+def index():
+    return "Khorma!"
+
+
+if __name__ == "__main__":
+    is_repl = os.environ.get("REPL_SLUG", False)
+    if is_repl:
+        app.run(debug=True, host="0.0.0.0")
+    else:
+        app.run(debug=True)
